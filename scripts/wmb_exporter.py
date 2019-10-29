dc241707d8d203229c186986039229d174f39558
DelleVelleD <dellevelled@yahoo.com>
2019-10-29 16:54:26 -0400

    Update wmb_exporter.py

--- scripts/wmb_exporter.py
+++ scripts/wmb_exporter.py
@@ -12,11 +12,13 @@ bl_info = {
 
 import bpy
 from bpy_extras.io_utils import ExportHelper,ImportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty, IntegerProperty
+from bpy.props import StringProperty, EnumProperty, IntProperty
 
-class ExportNierWMB(bpy.types.Operator):
+class ExportNierWMB(bpy.types.Operator, ExportHelper):
 	bl_idname = "export.wmb_data"
 	bl_label = "Export WMB Data"
+	bl_options = {'PRESET'}
+	filename_ext = ".wmb"
 	filepath = bpy.props.StringProperty(subtype="FILE_PATH")
 	
 	def execute(self, context):
@@ -24,9 +26,11 @@ class ExportNierWMB(bpy.types.Operator):
 		wmb_gen.WriteWMB(self.filepath, True)
 		return {'FINISHED'}
 
-#class ExportNierWTP(bpy.types.Operator):
+#class ExportNierWTP(bpy.types.Operator, ExportHelper):
 #	bl_idname = "export.wtp_data"
 #	bl_label = "Export WTP Data"
+#	bl_options = {'PRESET'}
+#	filename_ext = ".wtp"
 #	filepath = bpy.props.StringProperty(subtype="FILE_PATH")
 #	
 #	def execute(self, context):
@@ -34,9 +38,11 @@ class ExportNierWMB(bpy.types.Operator):
 #		wtp_gen.
 #		return {'FINISHED'}
 		
-#class ExportNierWTA(bpy.types.Operator):
+#class ExportNierWTA(bpy.types.Operator, ExportHelper):
 #	bl_idname = "export.wta_data"
 #	bl_label = "Export WTA Data"
+#	bl_options = {'PRESET'}
+#	filename_ext = ".wta"
 #	filepath = bpy.props.StringProperty(subtype="FILE_PATH")
 #	
 #	def execute(self, context):
@@ -44,21 +50,33 @@ class ExportNierWMB(bpy.types.Operator):
 #		wta_gen.
 #		return {'FINISHED'}
 	
-#class ExportNier(bpy.types.Operator):
+#class ExportNier(bpy.types.Operator, ExportHelper):
 #	bl_idname = "export.nier_data"
 #	bl_label = "Export Nier DAT/DTT"
+#	bl_options = {'PRESET'}
+#	filename_ext = ".dat"
 #	filepath = bpy.props.StringProperty(subtype="FILE_PATH")
 #	
 #	def execute(self, context):
 #		from blender2nier import wmb_gen
 #		from blender2nier import wtp_gen
 #		from blender2nier import wta_gen
-#		wmb_gen.WriteWMB(self.filepath, True)
+#		wmb_gen.WriteDAT(self.filepath, True)
 #		return {'FINISHED'}
 	
-def menu_func(self, context):
+def menu_func_export(self, context):
 	self.layout.operator(ExportNierWMB.bl_idname, text="WMB File for Nier: Automata (.wmb)")
 	#self.layout.operator(ExportNierWTP.bl_idname, text="WTP File for Nier: Automata (.wtp)")
 	#self.layout.operator(ExportNierWTA.bl_idname, text="WTA File for Nier: Automata (.wta)")
 	#self.layout.operator(ExportNier.bl_idname, text="DAT/DTT File for Nier: Automata (.dat/.dtt)")
 
+def register():
+	bpy.utils.register_module(__name__)
+	bpy.types.INFO_MT_file_export.append(menu_func_export)
+	
+def unregister():
+	bpy.utils.unregister_module(__name__)
+	bpy.types.INFO_MT_file_export.remove(menu_func_export)
+	
+if __name__ == '__main__':
+	register()
